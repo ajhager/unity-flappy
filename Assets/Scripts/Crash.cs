910032b1fae3d696da9970b961921c4465549861
Joseph Hager <ajhager@gmail.com>
2017-05-20 20:55:08 -0500

    Make spikes scroll with the ground and reset leaving the screen

--- Assets/Scripts/Crash.cs
+++ Assets/Scripts/Crash.cs
@@ -4,7 +4,7 @@ using UnityEngine;
 using UnityEngine.SceneManagement;
 
 public class Crash : MonoBehaviour {
-	void OnTriggerEnter2D(Collider2D other)
+    void OnTriggerEnter2D(Collider2D other)
     {
         SceneManager.LoadScene(0);
     }
--- Assets/Scripts/Scroller.cs
+++ Assets/Scripts/Scroller.cs
@@ -7,15 +7,17 @@ public class Scroller : MonoBehaviour
     public float tileSize;
 
     private Vector3 startPosition;
+    private float startTime;
 
     void Start()
     {
         startPosition = transform.position;
+        startTime = Time.time;
     }
 
     void Update()
     {
-        float newPosition = Mathf.Repeat(Time.time * scrollSpeed, tileSize);
+        float newPosition = Mathf.Repeat((Time.time - this.startTime) * scrollSpeed, tileSize);
         transform.position = startPosition - new Vector3(newPosition, 0, 0);
     }
 }
